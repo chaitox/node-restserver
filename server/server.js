a2c598d1aaef0e5abe0ec66e9d4e24c4afbdf01e
ricardo <ricardo.jul.88@gmail.com>
2020-08-20 17:04:22 -0400

    Inicio de google sign in

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 
 const app = express();
 
@@ -18,7 +20,8 @@ app.use(bodyParser.json())
 // Configuración global de rutas
 app.use(require('./routes/index'));
 
-
+//habilitar la carpeta publica
+app.use(express.static(path.resolve(__dirname,'../public'))); 
 
 mongoose.connect(process.env.URLDB,{
     useNewUrlParser: true,
